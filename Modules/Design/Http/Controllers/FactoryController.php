2cc3feb40e15c1b12f6b01c1d089caa667906349
weiqiang.du@majorbio.com <weiqiang.du@majorbio.com>
2020-05-08 09:25:11 +0800

    test

---
+++ Modules/Design/Http/Controllers/FactoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Design\Http\Controllers;
+
+
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+
+class FactoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        echo 123;die; 
+    }
+
+}
