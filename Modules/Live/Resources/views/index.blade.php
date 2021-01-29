4603e326052a2c3c91dc42982f8448fa53ff796c
weiqiang.du@majorbio.com <weiqiang.du@majorbio.com>
2021-01-29 10:13:39 +0800

    新增Live模块

---
+++ Modules/Live/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('live::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('live.name') !!}
+    </p>
+@endsection
